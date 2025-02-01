b8e9c95e97270ad6cac028cff33002dc3e560c35
Mohammed faisal S <faisalkabaddi@gmail.com>
2025-02-02 02:06:12 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ChatAnalyzer from "./pages/ChatAnalyzer.jsx";
 import Login from './pages/Login.jsx';
 import SignUpPage from './pages/SignUpPage.jsx';
+import Lander from './pages/Lander.jsx';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login />} /> {/* Home is now Login */}
+      <Route path="/" element={<Lander/>} /> {/* Home is now Login */}
+        <Route path="login" element={<Login />} /> {/* Home is now Login */}
         <Route path="/chat" element={<ChatAnalyzer />} />
         <Route path="/signup" element={<SignUpPage />} />
        
